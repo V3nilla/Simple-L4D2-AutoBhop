549d29fb22da1c7b14f42f18bbdb2a72c09d3303
V3nilla <48512277+V3nilla@users.noreply.github.com>
2022-03-09 19:26:17 +0400

    Add files via upload

---
+++ L4D2 AutoBhop/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace L4D2_AutoBhop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(73, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "AutoBhop";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(209, 41);
+            this.Controls.Add(this.checkBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "L4D2 Autobhop";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ L4D2 AutoBhop/Form1.cs
@@ -0,0 +1,62 @@
+﻿// Shit code starting here, enjoy!
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace L4D2_AutoBhop
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]  // Importing Key States from user32.dll
+        static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+
+        public static string Jump = "client.dll+0x7399C8"; // Maybe outdated offsets(2021)
+        public static string InAir = "client.dll+0x6C5E80"; // Maybe outdated offsets(2021)
+        Mem m = new Mem();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int procID = m.GetProcIdFromName("left4dead2");
+            if (procID > 0) // Checking game activity
+            {
+                m.OpenProcess(procID); // Open if found the process
+                Thread BH = new Thread(BHOP); // Starting BHOP Function if process is opened
+                BH.Start();
+            }
+        }
+        private void BHOP()
+        {
+            while (true)
+            {
+                if (checkBox1.Checked)
+                {
+                    if (GetAsyncKeyState(Keys.Space) < 0) // Holding spacebar
+                    {
+                        int ground = m.ReadInt(InAir);
+                        if (ground == 0) // If we are on the ground (0)
+                        {
+                            m.WriteMemory(Jump, "int", "5"); // Force Jump if ground value changed to 5
+                            Thread.Sleep(10);
+                            m.WriteMemory(Jump, "int", "4"); // Changing value to 4, so we can force jump again in the next time
+                        }
+                    }
+                }
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
